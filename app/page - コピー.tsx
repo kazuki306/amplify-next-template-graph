59c362f3fd3bf19af96f4db4f12de2f73f1170a7
pckk-aircon <kiyoshi.tabuchi@tk.pacific.co.jp>
2024-10-30 18:52:07 +0900

    field

---
+++ app/page - コピー.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { generateClient } from "aws-amplify/data";
+import type { Schema } from "@/amplify/data/resource";
+import "./../app/app.css";
+import { Amplify } from "aws-amplify";
+import outputs from "@/amplify_outputs.json";
+import "@aws-amplify/ui-react/styles.css";
+
+Amplify.configure(outputs);
+
+const client = generateClient<Schema>();
+
+export default function App() {
+  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+
+  function listTodos() {
+    client.models.Todo.observeQuery().subscribe({
+      next: (data) => setTodos([...data.items]),
+    });
+  }
+
+  const label = todos.map((todo) =>todo.label );
+  const value = todos.map((todo) =>todo.value );
+  //alert(label)
+
+
+  useEffect(() => {
+    listTodos();
+  }, []);
+
+  function createTodo() {
+    //下記を追加
+    //const todocontent = window.prompt("Todo content");
+    //const todolabel = window.prompt("label");
+    //const todovalue = window.prompt("value");
+  　//以下は修正。
+    client.models.Todo.create({
+      content: window.prompt("Todo content"),
+      //content: todocontent,
+      //label: todolabel,
+      //value: todovalue,
+    });
+  }
+
+  return (
+    <main>
+      <h1>My todos</h1>
+      <button onClick={createTodo}>+ new</button>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.content}&nbsp;{todo.label}&nbsp;{todo.value}</li>
+        ))}
+      </ul>
+      <div>
+        🥳 App successfully hosted. Try creating a new todo.
+        <br />
+        <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
+          Review next steps of this tutorial.
+        </a>
+      </div>
+    </main>
+  );
+}
