5119b8e1ff3bdbb61071ebb8b2124f9751aa95e3
pckk-aircon <kiyoshi.tabuchi@tk.pacific.co.jp>
2024-10-31 13:33:28 +0900

    grapf

--- app/page.tsx
+++ app/page.tsx
@@ -67,7 +67,11 @@ export default function App() {
       <PieChart
         series={[
           {
-            data: todos,
+            data: [
+              { id: 0, value: 10, label: 'series A' },
+              { id: 1, value: 15, label: 'series B' },
+              { id: 2, value: 20, label: 'series C' },
+            ],
           },
         ]}
         width={400}
