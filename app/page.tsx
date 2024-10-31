6862f553ee72153e2f8ef7ac97fbc4a331c66378
pckk-aircon <kiyoshi.tabuchi@tk.pacific.co.jp>
2024-10-31 14:28:10 +0900

    grapf

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
 
-import { LineChart } from '@mui/x-charts';
+//import { LineChart } from '@mui/x-charts';
 
 Amplify.configure(outputs);
 
@@ -60,17 +60,6 @@ export default function App() {
           </a>
         </div>
       </main>
-
-      <LineChart
-        xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
-        series={[
-          {
-            data: [2, 5.5, 2, 8.5, 1.5, 5],
-          },
-        ]}
-        width={500}
-        height={300}
-      />
 
     </> 
   );
