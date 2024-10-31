c0d0c1080c1410b011cdbf6e99671e50fd051e56
pckk-aircon <kiyoshi.tabuchi@tk.pacific.co.jp>
2024-10-31 14:49:16 +0900

    field

--- app/page.tsx
+++ app/page.tsx
@@ -42,24 +42,24 @@ export default function App() {
   }
 
   return (
- 
-    <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}&nbsp;{todo.label}&nbsp;{todo.value}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
-          Review next steps of this tutorial.
-        </a>
-      </div>
-    </main>
-
+    <> 
+      <main>
+        <h1>My todos</h1>
+        <button onClick={createTodo}>+ new</button>
+        <ul>
+          {todos.map((todo) => (
+            <li key={todo.id}>{todo.content}&nbsp;{todo.label}&nbsp;{todo.value}</li>
+          ))}
+        </ul>
+        <div>
+          🥳 App successfully hosted. Try creating a new todo.
+          <br />
+          <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
+            Review next steps of this tutorial.
+          </a>
+        </div>
+      </main>
+    </>
   );
 
 }
