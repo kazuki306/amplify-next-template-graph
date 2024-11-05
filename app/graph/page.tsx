76685d2548ee66b271feff89a1c54ccc14095904
pckk-aircon <kiyoshi.tabuchi@tk.pacific.co.jp>
2024-11-05 14:19:22 +0900

    graph

--- app/graph/page.tsx
+++ app/graph/page.tsx
@@ -1,6 +1,12 @@
 "use client";
 
-import Link from 'next/link';
+import { useState, useEffect } from "react";
+import { generateClient } from "aws-amplify/data";
+import type { Schema } from "@/amplify/data/resource";
+//import "./../app/app.css";
+import { Amplify } from "aws-amplify";
+import outputs from "@/amplify_outputs.json";
+import "@aws-amplify/ui-react/styles.css";
 
 import React from 'react';
 import { Line } from 'react-chartjs-2';
@@ -16,36 +22,40 @@ ChartJS.register(
   Legend
 );
 
-const data = {
-  labels: [1, 2, 3, 5, 8, 10],
-  datasets: [
-    {
-      label: 'Sample Data',
-      data: [2, 5.5, 2, 8.5, 1.5, 5],
-      fill: true, // areaを有効にする
-      backgroundColor: 'rgba(75,192,192,0.2)',
-      borderColor: 'rgba(75,192,192,1)',
-      tension: 0.4,
+
+export default function App() {
+
+  const data = {
+    labels: [1, 2, 3, 5, 8, 10],
+    datasets: [
+      {
+        label: 'Sample Data',
+        data: [2, 5.5, 2, 8.5, 1.5, 5],
+        fill: true, // areaを有効にする
+        backgroundColor: 'rgba(75,192,192,0.2)',
+        borderColor: 'rgba(75,192,192,1)',
+        tension: 0.4,
+      },
+    ],
+  };
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        display: true,
+      },
+      title: {
+        display: true,
+        text: 'Line Chart Example',
+      },
     },
-  ],
-};
+  };
 
-const options = {
-  responsive: true,
-  plugins: {
-    legend: {
-      display: true,
-    },
-    title: {
-      display: true,
-      text: 'Line Chart Example',
-    },
-  },
-};
+  return(
+    <>
+      <Line data={data} options={options} width={500} height={300} />;
+    </>
+  );
 
-const LineChartExample = () => {
-  return <Line data={data} options={options} width={500} height={300} />;
-};
-
-export default LineChartExample;
-
+}
