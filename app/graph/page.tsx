764bd31b35f6f38d1dbdc79c1d0413a203f14a0f
pckk-aircon <kiyoshi.tabuchi@tk.pacific.co.jp>
2024-11-05 13:46:20 +0900

    graph

--- app/graph/page.tsx
+++ app/graph/page.tsx
@@ -1,12 +1,42 @@
+"use client";
+
 import Link from 'next/link';
 
-//import "./app.css";
+import {
+  Chart as ChartJS,
+  RadialLinearScale,
+  PointElement,
+  LineElement,
+  Filler,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import * as React from 'react';
+import { Radar } from 'react-chartjs-2';
 
-export default function Home() {
-  return (
-    <>
-      <h1>グラフページ</h1>
-    </>
-
+function ChartJsRader() {
+  ChartJS.register(
+    RadialLinearScale,
+    PointElement,
+    LineElement,
+    Filler,
+    Tooltip,
+    Legend,
   );
+
+  const data = {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+    datasets: [
+      {
+        label: '# of Votes',
+        data: [80, 50, 30, 40, 100, 20],
+        backgroundColor: 'rgba(96, 109, 194, 0.2)',
+        borderColor: 'rgba(96, 109, 194, 1)',
+        borderWidth: 1,
+      },
+    ],
+  };
+  return <Radar data={data} />;
 }
+
+export default ChartJsRader;
